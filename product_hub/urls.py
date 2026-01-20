7744d2eb5ffc2c3216ae50348cacb0c65cf77d05
elkaz84 <eldadkaz@gmail.com>
2026-01-20 10:58:20 +0000

    Add Bootstrap styling and responsive product list view

--- product_hub/urls.py
+++ product_hub/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include # Add include here
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('catalog.urls')),   # send root URL to catalog app
+    path('', include('catalog.urls')), # This makes catalog the home page
 ]
